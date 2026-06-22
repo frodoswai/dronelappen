bdda26be42d763b9256d48f693419c9ac726fc6b
Dobby <frode74@gmail.com>
2026-06-22 15:18:39 +0200

    Add Meta Pixel (1025209573360224) to app + blog, allow facebook in CSP

--- scripts/build-blogg.mjs
+++ scripts/build-blogg.mjs
@@ -112,6 +112,21 @@ ${ogImage ? `<meta property="og:image" content="${ogImage}">
 ${FONTS}
 <style>${CSS}</style>
 ${jsonld ? `<script type="application/ld+json">${JSON.stringify(jsonld)}</script>` : ''}
+<!-- MacMiniHub Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1025209573360224');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1025209573360224&ev=PageView&noscript=1"/></noscript>
+<!-- End MacMiniHub Meta Pixel Code -->
 </head>
 <body>
 <header class="site">
